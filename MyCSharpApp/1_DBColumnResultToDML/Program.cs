86024424e7d7fad8a801a0c9e3d221f509ab5b06
Subha Deb <subdeb@deloitte.com>
2020-09-23 20:24:03 +0530

    New Projects

---
+++ MyCSharpApp/1_DBColumnResultToDML/Program.cs
@@ -0,0 +1,469 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    public const string InputFilePath = @"C:\Temp\InputQueryResult.txt";
+    public const string OutputFilePath = @"C:\Temp\OutputDMLQuery.txt";
+
+    public const string TableNameForPlainText = "DataCollection.Hell";  //"@TableName123";
+
+
+    //select BENSequenceNumber,SSN,CaseNumber,IndividualId,ClaimNumber,IsIncomeUpdated,IncomeUpdateReason,*  from Interface.BENDEXMaster where IndividualId = 803001209
+
+    public const string ExistingSSNForBendex = "544662042";
+    public const string ExistingCaseNumberForBendex = "400345112";
+    public const string ExistingIndividualIdForBendex = "800840232";//---Goes to Income Table
+    public const string ExistingClaimNumberForBendex = "544689894A ";//---Goes to Income Table
+
+
+    public const string SSNForBendexDev5 = "544662042";
+    public const string CaseNumberForBendexDev5 = "100016030";
+    public const string IndividualIdForBendexDev5 = "990242795";//---Goes to Income Table
+    public const string ClaimNumberForBendexDev5 = "544689894A";//---Goes to Income Table
+
+
+    public static void Main(string[] args)
+    {
+        InsertStatementCreatorFromPlainText();
+        //UpdateStatementCreatorFromPlainText();
+        //InsertStatementCreatorFromPlainTextSDX();
+        /*InsertStatementCreatorFromPlainTextSDX();*/
+
+    }
+    static void InsertStatementCreatorFromPlainText()
+    {
+
+        List<string> listStrLineElements = new List<string>();
+        StringBuilder sbQuery = new StringBuilder();
+        //Step 1/4: Read the Input File and Push the Line Items into a List of String(listStrLineElements)
+        var fileStream = new FileStream(InputFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+        {
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                listStrLineElements.Add(line);
+            }
+            streamReader.Close();
+        }
+        //Step 2/4 Create the Query to list the table and the column names with 8 columns in one line(using newLineCounter)
+        if (listStrLineElements.Count > 1)
+        {
+            var columnNamesArray = listStrLineElements[0].Split('\t');
+            var rowValuesArray = listStrLineElements[1].Split('\t');
+            sbQuery.Append("INSERT INTO " + TableNameForPlainText + " ");
+            sbQuery.Append("(");
+            int newLineCounter = 0;
+            for (int i = 0; i < columnNamesArray.Length; i++)
+            {
+                var colName = columnNamesArray[i];
+                if (i != columnNamesArray.Length - 1)
+                {
+                    sbQuery.Append(colName + ",");
+                    newLineCounter++;
+                }
+                else
+                {
+                    sbQuery.Append(colName + ")");
+                    newLineCounter++;
+                }
+
+                if (newLineCounter % 8 == 0)
+                {
+                    sbQuery.Append("\n");
+                }
+
+            }
+
+            sbQuery.Append("\n");
+            sbQuery.Append("VALUES(");
+            newLineCounter = 0;
+            //Step 3/4 Create the Query to insert values into the correspondinig column names with 8 columns in one line(using newLineCounter)
+            for (int i = 0; i < rowValuesArray.Length; i++)
+            {
+                var itemValue = rowValuesArray[i];
+                if (i != rowValuesArray.Length - 1)//If i is not the last column put Comma after each element
+                {
+                    if (itemValue == "NULL")
+                    {
+                        sbQuery.Append(itemValue + ",");
+                    }
+                    else if (itemValue.Contains("0x00"))//For RowVersionStamp
+                    {
+                        sbQuery.Append("NULL" + ",");
+                    }
+                    else
+                    {
+                        sbQuery.Append("'" + itemValue + "',");
+                    }
+
+                    newLineCounter++;
+                }
+                else//It is the Last Column(For Appending Closing First Bracket instead of Comma)
+                {
+                    if (itemValue == "NULL")
+                    {
+                        sbQuery.Append(itemValue + ")");
+                    }
+                    else if (itemValue.Contains("0x00"))//For RowVersionStamp
+                    {
+                        sbQuery.Append("NULL" + ")");
+                    }
+                    else
+                    {
+                        sbQuery.Append("'" + itemValue + "')");
+                    }
+                    newLineCounter++;
+                }
+
+                if (newLineCounter % 8 == 0)
+                {
+                    sbQuery.Append("\n");
+                }
+            }
+
+        }
+        sbQuery.Append("\n");
+        sbQuery.Append("SELECT SCOPE_IDENTITY() AS NewlyInsertedIdentityValue");
+        Console.WriteLine(sbQuery);
+        //Step 4/4 Write the String Builder into the O/P Fiile
+        var fileStreamOP = new FileStream(OutputFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        //File.WriteAllText(OutputFilePath, "");
+        using (var streamWriter = new StreamWriter(fileStreamOP, Encoding.UTF8))
+        {
+            streamWriter.Write(sbQuery);
+        }
+
+        Console.WriteLine("Write Here");
+
+    }
+    static void UpdateStatementCreatorFromPlainText()
+    {
+        List<string> listStrLineElements = new List<string>();
+        StringBuilder sbQuery = new StringBuilder();
+        //Step 1/3: Read the Input File and Push the Line Items into a List of String(listStrLineElements)
+        var fileStream = new FileStream(InputFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+        {
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                listStrLineElements.Add(line);
+            }
+            streamReader.Close();
+        }
+        //Step 2/3 Create the Query to list the table and the column names = values with 3 columns in one line(using newLineCounter)
+        if (listStrLineElements.Count > 1)
+        {
+            var columnNamesArray = listStrLineElements[0].Split('\t');
+            var rowValuesArray = listStrLineElements[1].Split('\t');
+            sbQuery.Append("UPDATE @TableName ");
+            sbQuery.Append("\n");
+            sbQuery.Append("SET ");
+            int newLineCounter = 0;
+            for (int i = 0; i < columnNamesArray.Length; i++)
+            {
+                var colName = columnNamesArray[i];
+                var itemValue = rowValuesArray[i];
+                if (i != columnNamesArray.Length - 1)
+                {
+                    if (itemValue != "NULL")
+                    {
+                        sbQuery.Append(colName + " = '" + itemValue + "',");
+                    }
+                    else
+                    {
+                        sbQuery.Append(colName + " = " + itemValue + ",");
+                    }
+
+                    newLineCounter++;
+                }
+                else
+                {
+                    if (itemValue != "NULL")
+                    {
+                        sbQuery.Append(colName + " = '" + itemValue + "'");
+                    }
+                    else
+                    {
+                        sbQuery.Append(colName + " = " + itemValue);
+                    }
+                    newLineCounter++;
+                }
+
+                if (newLineCounter % 3 == 0)
+                {
+                    sbQuery.Append("\n");
+                }
+            }
+            sbQuery.Append("\n");
+            sbQuery.Append("WHERE 1 = 0 //Put the condition here for Update");
+            sbQuery.Append("\n");
+        }
+        Console.WriteLine(sbQuery);
+        //Step 3/3 Write the String Builder into the O/P Fiile
+        var fileStreamOP = new FileStream(OutputFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        using (var streamWriter = new StreamWriter(fileStreamOP, Encoding.UTF8))
+        {
+            streamWriter.Write(sbQuery);
+        }
+
+        Console.WriteLine("Write Here");
+
+    }
+    static void InsertStatementCreatorFromPlainTextBendex()
+    {
+        //SSN(304465469),CaseNumber(100014582),IndividualId(990234323),ClaimNumber(304465469A),IsIncomeUpdated-R5(NULL),IncomeUpdateReason-R5(NULL),RowVersionStamp-R5(NULL)
+
+
+        List<string> listStrLineElements = new List<string>();
+        StringBuilder sbQuery = new StringBuilder();
+        //Step 1/4: Read the Input File and Push the Line Items into a List of String(listStrLineElements)
+        var fileStream = new FileStream(InputFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+        {
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                listStrLineElements.Add(line);
+            }
+            streamReader.Close();
+        }
+        //Step 2/4 Create the Query to list the table and the column names with 8 columns in one line(using newLineCounter)
+        if (listStrLineElements.Count > 1)
+        {
+            var columnNamesArray = listStrLineElements[0].Split('\t');
+            var rowValuesArray = listStrLineElements[1].Split('\t');
+            if (columnNamesArray[0] == "BENDEXMasterId")
+            {
+                sbQuery.Append("INSERT INTO Interface.BENDEXMaster ");
+            }
+            if (columnNamesArray[0] == "BENSequenceNumber")
+            {
+                sbQuery.Append("INSERT INTO Interface.BENDEXStaging ");
+            }
+
+            sbQuery.Append("(");
+            int newLineCounter = 0;
+            for (int i = 0; i < columnNamesArray.Length; i++)
+            {
+                var colName = columnNamesArray[i];
+                if (i != columnNamesArray.Length - 1)
+                {
+                    sbQuery.Append(colName + ",");
+                    newLineCounter++;
+                }
+                else
+                {
+                    sbQuery.Append(colName + ")");
+                    newLineCounter++;
+                }
+
+                if (newLineCounter % 8 == 0)
+                {
+                    sbQuery.Append("\n");
+                }
+
+            }
+
+            sbQuery.Append("\n");
+            sbQuery.Append("VALUES(");
+            newLineCounter = 0;
+            //Step 3/4 Create the Query to insert values into the correspondinig column names with 8 columns in one line(using newLineCounter)
+            for (int i = 0; i < rowValuesArray.Length; i++)
+            {
+                var itemValue = rowValuesArray[i];
+                if (i != rowValuesArray.Length - 1)//If i is not the last column put Comma after each element
+                {
+                    if (itemValue == "NULL")
+                    {
+                        sbQuery.Append(itemValue + ",");
+                    }
+                    else if (itemValue.Contains("0x00"))//For RowVersionStamp
+                    {
+                        sbQuery.Append("NULL" + ",");
+                    }
+                    else
+                    {
+                        sbQuery.Append("'" + itemValue + "',");
+                    }
+
+                    newLineCounter++;
+                }
+                else//It is the Last Column(For Appending Closing First Bracket instead of Comma)
+                {
+                    if (itemValue == "NULL")
+                    {
+                        sbQuery.Append(itemValue + ")");
+                    }
+                    else if (itemValue.Contains("0x00"))//For RowVersionStamp
+                    {
+                        sbQuery.Append("NULL" + ")");
+                    }
+                    else
+                    {
+                        sbQuery.Append("'" + itemValue + "')");
+                    }
+                    newLineCounter++;
+                }
+
+                if (newLineCounter % 8 == 0)
+                {
+                    sbQuery.Append("\n");
+                }
+            }
+
+            sbQuery.Append("\n");
+            sbQuery.Append("SELECT SCOPE_IDENTITY() AS NewlyInsertedIdentityValue");
+
+            //Replacing the SSN/Claimnumbers with Dev5 Compatibility
+            sbQuery.Replace(ExistingSSNForBendex, SSNForBendexDev5);
+            sbQuery.Replace(ExistingCaseNumberForBendex, CaseNumberForBendexDev5);
+            sbQuery.Replace(ExistingIndividualIdForBendex, IndividualIdForBendexDev5);
+            sbQuery.Replace(ExistingClaimNumberForBendex, ClaimNumberForBendexDev5);
+
+        }
+        Console.WriteLine(sbQuery);
+        //Step 4/4 Write the String Builder into the O/P Fiile
+        var fileStreamOP = new FileStream(OutputFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        //File.WriteAllText(OutputFilePath, "");
+        using (var streamWriter = new StreamWriter(fileStreamOP, Encoding.UTF8))
+        {
+            streamWriter.Write(sbQuery);
+        }
+
+        Console.WriteLine("Write Here");
+
+    }
+    static void InsertStatementCreatorFromPlainTextSDX()
+    {
+        //SSN(304465469),CaseNumber(100014582),IndividualId(990234323),ClaimNumber(304465469A),IsIncomeUpdated-R5(NULL),IncomeUpdateReason-R5(NULL),RowVersionStamp-R5(NULL)
+
+
+        List<string> listStrLineElements = new List<string>();
+        StringBuilder sbQuery = new StringBuilder();
+        //Step 1/4: Read the Input File and Push the Line Items into a List of String(listStrLineElements)
+        var fileStream = new FileStream(InputFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+        {
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                listStrLineElements.Add(line);
+            }
+            streamReader.Close();
+        }
+        //Step 2/4 Create the Query to list the table and the column names with 8 columns in one line(using newLineCounter)
+        if (listStrLineElements.Count > 1)
+        {
+            var columnNamesArray = listStrLineElements[0].Split('\t');
+            var rowValuesArray = listStrLineElements[1].Split('\t');
+            if (columnNamesArray[0] == "SDXMasterId")
+            {
+                sbQuery.Append("INSERT INTO Interface.SDXMaster ");
+            }
+            if (columnNamesArray[0] == "SDXSequenceNumber")
+            {
+                sbQuery.Append("INSERT INTO Interface.SDXStaging ");
+            }
+
+            sbQuery.Append("(");
+            int newLineCounter = 0;
+            for (int i = 0; i < columnNamesArray.Length; i++)
+            {
+                var colName = columnNamesArray[i];
+                if (i != columnNamesArray.Length - 1)
+                {
+                    sbQuery.Append(colName + ",");
+                    newLineCounter++;
+                }
+                else
+                {
+                    sbQuery.Append(colName + ")");
+                    newLineCounter++;
+                }
+
+                if (newLineCounter % 8 == 0)
+                {
+                    sbQuery.Append("\n");
+                }
+
+            }
+
+            sbQuery.Append("\n");
+            sbQuery.Append("VALUES(");
+            newLineCounter = 0;
+            //Step 3/4 Create the Query to insert values into the correspondinig column names with 8 columns in one line(using newLineCounter)
+            for (int i = 0; i < rowValuesArray.Length; i++)
+            {
+                var itemValue = rowValuesArray[i];
+                if (i != rowValuesArray.Length - 1)//If i is not the last column put Comma after each element
+                {
+                    if (itemValue == "NULL")
+                    {
+                        sbQuery.Append(itemValue + ",");
+                    }
+                    else if (itemValue.Contains("0x00"))//For RowVersionStamp
+                    {
+                        sbQuery.Append("NULL" + ",");
+                    }
+                    else
+                    {
+                        sbQuery.Append("'" + itemValue + "',");
+                    }
+
+                    newLineCounter++;
+                }
+                else//It is the Last Column(For Appending Closing First Bracket instead of Comma)
+                {
+                    if (itemValue == "NULL")
+                    {
+                        sbQuery.Append(itemValue + ")");
+                    }
+                    else if (itemValue.Contains("0x00"))//For RowVersionStamp
+                    {
+                        sbQuery.Append("NULL" + ")");
+                    }
+                    else
+                    {
+                        sbQuery.Append("'" + itemValue + "')");
+                    }
+                    newLineCounter++;
+                }
+
+                if (newLineCounter % 8 == 0)
+                {
+                    sbQuery.Append("\n");
+                }
+            }
+
+            sbQuery.Append("\n");
+            sbQuery.Append("SELECT SCOPE_IDENTITY() AS NewlyInsertedIdentityValue");
+
+            //Replacing the SSN/Claimnumbers with Dev5 Compatibility
+            //sbQuery.Replace(ExistingSSNForBendex, SSNForBendexDev5);
+            //sbQuery.Replace(ExistingCaseNumberForBendex, CaseNumberForBendexDev5);
+            //sbQuery.Replace(ExistingIndividualIdForBendex, IndividualIdForBendexDev5);
+            //sbQuery.Replace(ExistingClaimNumberForBendex, ExistingClaimNumberForBendex);
+
+        }
+        Console.WriteLine(sbQuery);
+        //Step 4/4 Write the String Builder into the O/P Fiile
+        var fileStreamOP = new FileStream(OutputFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        //File.WriteAllText(OutputFilePath, "");
+        using (var streamWriter = new StreamWriter(fileStreamOP, Encoding.UTF8))
+        {
+            streamWriter.Write(sbQuery);
+        }
+
+        Console.WriteLine("Write Here");
+
+    }
+}
+
---
+++ MyCSharpApp/2_DefectFormatterApp/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    //FOR DEV, IN THE METHOD WriteToOutputFile set writeToFile = false, for Prod it is true
+    public const string InputFilePath = @"C:\Users\subdeb\Documents\ProjectWP\DefectFixingSIT\00Input_Copy.txt";
+    public const string OutputFilePath = @"C:\Users\subdeb\Documents\ProjectWP\DefectFixingSIT\00DefectListSIT.txt";
+    public const string OutputDirectoryPath = @"C:\Users\subdeb\Documents\ProjectWP\DefectFixingSIT\";
+
+
+    //Static Common Variables
+    static List<string> DefectList;
+    static int LastSerialCount;
+    static List<string> ExistingBugIdList;
+    static List<string> InsertedSerialCount;
+
+    static void Main(string[] args)
+    {
+        ReadFromInputFile();//This Method reads the input file and make entries to the DefectList common variable
+        ReadDataFromOutputFile();//This method gets the last serial count from o/p file and make entries for all the BugIds to ExistingBugIdList common variable
+        if (DefectList.Any())
+        {
+            InsertedSerialCount = new List<string>();
+            int tempBugIdAssignedEmail = 0;
+            int.TryParse(DefectList.FirstOrDefault().Substring(0, 6), out tempBugIdAssignedEmail);//For Email Bug Assignments/Triage, the first 6chars will be the Bug Id.
+
+            if (DefectList.FirstOrDefault().Contains("Work item Changed"))//If it is Autmated TFSIE Email, it will contain the mentioned text
+            {
+                FormatOutputFileForTFSIE_EMail();
+            }
+            else if (tempBugIdAssignedEmail > 100000 && (DefectList.FirstOrDefault().Substring(6, 4) != "\tBug"))//Currently All BugIds are of 6 Digits greater than 100000
+            {
+                FormatOutputFileForAssignedEMail();
+            }
+            else
+            {
+                FormatOutputFileForTFS();
+            }
+
+            if (InsertedSerialCount.Count > 0)
+            {
+                Console.WriteLine(InsertedSerialCount.Count + " Record(s) Inserted with Generated Serial Count: " + string.Join(", ", InsertedSerialCount));
+            }
+            else
+            {
+                Console.WriteLine("No Records Inserted. The Records might already exist");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No Records Inserted. Reverify the Input File");
+        }
+        //var EXEFilePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+        //System.Diagnostics.Process.Start(EXEFilePath);
+        Console.ReadLine();
+    }
+    static void WriteToOutputFile(string strLineItem)
+    {
+        bool writeToFile = true;//Make it true for Writing to File/During Deployment and false while debugging
+        if (writeToFile)
+        {
+            InsertedSerialCount.Add(strLineItem.Substring(0, 3));
+            File.AppendAllText(OutputFilePath, Environment.NewLine);
+            File.AppendAllText(OutputFilePath, strLineItem);
+            Directory.CreateDirectory(OutputDirectoryPath + strLineItem);
+        }
+    }
+
+    static void ReadFromInputFile()
+    {
+        DefectList = new List<string>();
+        var fileStream = new FileStream(InputFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+        {
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                if (line.Substring(0, 2) != "ID")//Ignore the first line of Header in the Inputfile
+                {
+                    DefectList.Add(line);
+                }
+
+            }
+            streamReader.Close();
+        }
+    }
+    static void ReadDataFromOutputFile()
+    {
+        ExistingBugIdList = new List<string>();
+        var fileStream = new FileStream(OutputFilePath, FileMode.OpenOrCreate, FileAccess.Read);
+        using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+        {
+            string LastLine = "";
+            while (!streamReader.EndOfStream)
+            {
+                var currentLineItem = streamReader.ReadLine();
+                if (!string.IsNullOrEmpty(currentLineItem))
+                {
+                    var currentBugId = GetBugIdFromLineItem(currentLineItem);
+                    ExistingBugIdList.Add(currentBugId);
+                    LastLine = currentLineItem;
+                }
+            }
+            LastSerialCount = Convert.ToInt32(LastLine.Substring(0, 3));
+            streamReader.Close();
+        }
+    }
+    static void FormatOutputFileForTFS()
+    {
+
+        foreach (var strLineItemFromInputFile in DefectList)
+        {
+            var ElementsOfDefectArray = strLineItemFromInputFile.Split('\t');
+            StringBuilder sbLineItem = new StringBuilder();
+            if (!ExistingBugIdList.Contains(ElementsOfDefectArray[0]))
+            {
+                LastSerialCount++;
+                sbLineItem.Append(LastSerialCount);
+                sbLineItem.Append("_" + ElementsOfDefectArray[0]);//0th Element Contains the BugId
+                sbLineItem.Append("_" + ElementsOfDefectArray[1]);//1st Elemeent Contains the String 'Bug'
+                var DefectTitleRemovingSpecialChars = Regex.Replace(ElementsOfDefectArray[2], @"[^0-9a-zA-Z ]+", "");//2nd Element Contains the BugTitle
+                DefectTitleRemovingSpecialChars = DefectTitleRemovingSpecialChars.Length > 100 ? DefectTitleRemovingSpecialChars.Substring(0, 100) : DefectTitleRemovingSpecialChars;
+                sbLineItem.Append("_" + DefectTitleRemovingSpecialChars);
+                string strLineItem = sbLineItem.ToString();
+                WriteToOutputFile(strLineItem);
+            }
+        }
+    }
+    static void FormatOutputFileForTFSIE_EMail()
+    {
+        var strLineItemFromInputFile = DefectList.FirstOrDefault();
+        var indexOfBug = strLineItemFromInputFile.IndexOf("Bug");
+        var startIndexOfBugId = indexOfBug + 4;//Will count till Bug[Space]: 4 Characters
+        var strBugId = strLineItemFromInputFile.Substring(startIndexOfBugId, 6);//Bug is of 6 Digits
+        if (!ExistingBugIdList.Contains(strBugId))
+        {
+            StringBuilder sbLineItem = new StringBuilder();
+            var startIndexOfBugTitle = startIndexOfBugId + 9;// [ - ] is there after the Bug Id before Bug Title
+            var strBugTitle = strLineItemFromInputFile.Substring(startIndexOfBugTitle);
+            strBugTitle = Regex.Replace(strBugTitle, @"[^0-9a-zA-Z ]+", "");
+            if (strBugTitle.Length > 100)
+            {
+                strBugTitle = strBugTitle.Substring(0, 100);
+            }
+            LastSerialCount++;
+            sbLineItem.Append(LastSerialCount);
+            sbLineItem.Append("_" + strBugId);
+            sbLineItem.Append("_" + "Bug");
+            sbLineItem.Append("_" + strBugTitle);
+            string strLineItem = sbLineItem.ToString();
+            WriteToOutputFile(strLineItem);
+
+        }
+    }
+    static void FormatOutputFileForAssignedEMail()
+    {
+        foreach (var strLineItemFromInputFile in DefectList)
+        {
+            var elementsOfDefectArray = strLineItemFromInputFile.Split('\t');//It will have two elements, BugId and T
+            var strBugId = elementsOfDefectArray[0];
+            var strBugTitle = elementsOfDefectArray[1];
+            if (!ExistingBugIdList.Contains(strBugId))
+            {
+                StringBuilder sbLineItem = new StringBuilder();
+                strBugTitle = Regex.Replace(strBugTitle, @"[^0-9a-zA-Z ]+", "");
+                if (strBugTitle.Length > 100)
+                {
+                    strBugTitle = strBugTitle.Substring(0, 100);
+                }
+                LastSerialCount++;
+                sbLineItem.Append(LastSerialCount);
+                sbLineItem.Append("_" + strBugId);
+                sbLineItem.Append("_" + "Bug");
+                sbLineItem.Append("_" + strBugTitle);
+                string strLineItem = sbLineItem.ToString();
+                WriteToOutputFile(strLineItem);
+
+            }
+        }
+    }
+    static string GetBugIdFromLineItem(string LineItem)
+    {
+        var LineItemElementsArray = LineItem.Split('_');
+        return LineItemElementsArray[1];//The Second Array Element is the Bug Id
+    }
+}
