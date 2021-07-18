0f7bdf0d7f82a5303066d2cf5b2ffca72e1568ec
Subha Deb <subdeb@deloitte.com>
2021-07-18 19:44:08 +0530

    Updates to Practice codes and Rebranded PanelMember Operations

--- MyCSharpApp/10_PanelMembersOperationsCount/Program.cs
+++ MyCSharpApp/10_PanelMembersOperationsCount/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 /*
 
+    ReadResourceFile(): Reads the Resource FIle and populated the Project path which is later appended with InputFileRelativePath/OutputFileRelativePath
+    ReadFromInputFile(): Reads the Contents from two Input files stored in the location InputFileRelativePath and push all the lines to PanelMembersLineInput1 and PanelMembersLineInput2
+    FilterDuplicatesInBothLineInputs(): Finds Duplicate Emails of both the Inputs to create PanelMembersLineInput1Unique and PanelMembersLineInput2Unique and updates sbTextToWriteInOutput for the same.
+    FindCommonAndDifferentPanelMembers(): Updates to the three important Lists: PanelMembersCommonInBoth,PanelMembersInput2NotInInput1 and PanelMembersInput1NotInInput2 and updates sbTextToWriteInOutput.
 
 
+    Assume InputFile1 is the existing Sharepoint list, InputFile2 is the new one to be Added to the portal
+    InputFile1 Format: It should have: Id  [/t]  Email   [/t]   Level
+    InputFile2 Format: It should have: Id  [/t]  Email   [/t]   Level (If Id is not present keep it -1 or -something)
 
 
 
@@ -17,122 +27,223 @@ using System.Threading.Tasks;
 
 class Program
 {
+    public static string RepositoryProjectsPath = string.Empty;
+    public const string InputFile1RelativePath = @"10_PanelMembersOperationsCount\bin\Debug\InputOutput_PanelMemberOperations\InputFile1.txt";
+    public const string InputFile2RelativePath = @"10_PanelMembersOperationsCount\bin\Debug\InputOutput_PanelMemberOperations\InputFile2.txt";
+    public const string OutputFileRelativePath = @"10_PanelMembersOperationsCount\bin\Debug\InputOutput_PanelMemberOperations\OutputFile.txt";
 
-    public const string InputFilePath1 = @"C:\\SubhaTemp\TempInputFile1.txt";
-    public const string InputFilePath2 = @"C:\\SubhaTemp\TempInputFile2.txt";
-    public const string OutputFilePath = @"C:\\SubhaTemp\TempOutputFile.txt";
-    public const string DirectoryFileCreationPath = @"C:\SubhaTemp\UdemyReactFiles\";
-    static List<EmailLevel> LineInput1 = new List<EmailLevel>();
-    static List<EmailLevel> LineInput2 = new List<EmailLevel>();
+
+
+    static List<PanelMemberModel> PanelMembersLineInput1 = new List<PanelMemberModel>();
+    static List<PanelMemberModel> PanelMembersLineInput2 = new List<PanelMemberModel>();
+    static List<PanelMemberModel> PanelMembersLineInput1Unique = new List<PanelMemberModel>();
+    static List<PanelMemberModel> PanelMembersLineInput2Unique = new List<PanelMemberModel>();
+    static List<PanelMemberModel> PanelMembersCommonInBoth = new List<PanelMemberModel>();
+    static List<PanelMemberModel> PanelMembersInput2NotInInput1 = new List<PanelMemberModel>();
+    static List<PanelMemberModel> PanelMembersInput1NotInInput2 = new List<PanelMemberModel>();
+
+    static StringBuilder sbTextToWriteInOutput = new StringBuilder();
 
     static void Main(string[] args)
     {
+        ReadResourceFile();
         ReadFromInputFile();
-        ProocessAndWriteToOutputFile();
+        bool isWriteToFileFilterDuplicates = true;
+        FilterDuplicatesInBothLineInputs(isWriteToFileFilterDuplicates);
+        bool isWriteToFileFindCommonDifferent = true;
+        FindCommonAndDifferentPanelMembers(isWriteToFileFindCommonDifferent);
+        if (sbTextToWriteInOutput.Length > 0)
+        {
+            WriteToFile();
+        }
+    }
+    static void ReadResourceFile()
+    {
+        //Make sure the resourFile have access modifier as public and System.Forms.Dll is imported for ResXResourceReader to work
+        var resourceFileRelativePath = @"MyCSharpApp\MyCSharpApp\MyCSharpApp\Resources\ResourcesFile.resx";
+        var executingAssemblyPath = Assembly.GetExecutingAssembly().Location;
+        var firstIndexOfMyCSharpApp = executingAssemblyPath.IndexOf("MyCSharpApp");
+        string resourceFilePath = executingAssemblyPath.Substring(0, firstIndexOfMyCSharpApp) + resourceFileRelativePath;
+        ResXResourceReader rsxr = new ResXResourceReader(resourceFilePath);
+        foreach (DictionaryEntry de in rsxr)
+        {
+            if (de.Key.ToString() == "RepositoryProjectsPath_" + Environment.MachineName)
+            {
+                RepositoryProjectsPath = de.Value.ToString();
+            }
+        }
+        rsxr.Close();
     }
     static void ReadFromInputFile()
     {
-        var fileStream1 = new FileStream(InputFilePath1, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        var fileStream1 = new FileStream(RepositoryProjectsPath + InputFile1RelativePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
         using (var streamReader = new StreamReader(fileStream1, Encoding.UTF8))
         {
             string line;
             while ((line = streamReader.ReadLine()) != null)
             {
                 var splittedArray = line.Split('\t');
-                EmailLevel el = new EmailLevel() { Level = splittedArray[0], Email = splittedArray[1].ToLower() };
-                LineInput1.Add(el);
+                PanelMemberModel el = new PanelMemberModel() { Id = splittedArray[0], Email = splittedArray[1].ToLower(), Level = splittedArray[2] };
+                PanelMembersLineInput1.Add(el);
             }
             streamReader.Close();
         }
-        var fileStream2 = new FileStream(InputFilePath2, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        var fileStream2 = new FileStream(RepositoryProjectsPath + InputFile2RelativePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
         using (var streamReader = new StreamReader(fileStream2, Encoding.UTF8))
         {
             string line;
             while ((line = streamReader.ReadLine()) != null)
             {
                 var splittedArray = line.Split('\t');
-                EmailLevel el = new EmailLevel() { Level = splittedArray[0], Email = splittedArray[1].ToLower() };
-                LineInput2.Add(el);
+                PanelMemberModel el = new PanelMemberModel() { Id = splittedArray[0], Email = splittedArray[1].ToLower(), Level = splittedArray[2] };
+                PanelMembersLineInput2.Add(el);
             }
             streamReader.Close();
         }
     }
-    static void ProocessAndWriteToOutputFile()
+
+    public static void FilterDuplicatesInBothLineInputs(bool isWriteToFile)
     {
-        //LineInput1 is the new list, 2 is the old one
-
-
-        var commonInBoth = new List<EmailLevel>();
-        var commonInBoth2 = new List<EmailLevel>();
-        var neeeToAdd = new List<EmailLevel>();
-        var neeeToInactivate = new List<EmailLevel>();
-
-
-        var dupes1 = LineInput1.GroupBy(x => new { x.Email })
+        var dupes1 = PanelMembersLineInput1.GroupBy(x => new { x.Email })
                    .Where(x => x.Skip(1).Any()).ToList();
 
-        Console.WriteLine("Duplicates in List1: " + dupes1.Count());
 
-
-        var dupes2 = LineInput2.GroupBy(x => new { x.Email })
+        var dupes2 = PanelMembersLineInput2.GroupBy(x => new { x.Email })
                    .Where(x => x.Skip(1).Any()).ToList();
 
-        Console.WriteLine("Duplicates in List2 : " + dupes2.Count());
-
-        var LineInput2Unique = new List<EmailLevel>();
-        foreach (var data in LineInput2)
+        foreach (var data in PanelMembersLineInput1)
         {
-            if (!LineInput2Unique.Any(x => x.Email == data.Email))
+            if (!PanelMembersLineInput1Unique.Any(x => x.Email == data.Email))
             {
-                LineInput2Unique.Add(data);
+                PanelMembersLineInput1Unique.Add(data);
             }
         }
-        Console.WriteLine("Unique List2 count: " + LineInput2Unique.Count());
 
-
-
-
-        foreach (var data in LineInput1)
+        foreach (var data in PanelMembersLineInput2)
         {
-            if (LineInput2.Any(x => x.Email == data.Email))
+            if (!PanelMembersLineInput2Unique.Any(x => x.Email == data.Email))
             {
-                commonInBoth.Add(data);
+                PanelMembersLineInput2Unique.Add(data);
+            }
+        }
+       
+
+        if (isWriteToFile)
+        {
+            sbTextToWriteInOutput.AppendLine("Total PanelMembers in Input1: " + PanelMembersLineInput1.Count());
+            sbTextToWriteInOutput.AppendLine("Total PanelMembers in Input2: " + PanelMembersLineInput2.Count());
+
+            sbTextToWriteInOutput.AppendLine();
+            sbTextToWriteInOutput.AppendLine("Duplicates in InputFile1: " + dupes1.Count());
+            sbTextToWriteInOutput.AppendLine("Duplicates in InputFile2: " + dupes2.Count());
+
+            if (dupes1.Count() > 0)
+            {
+                sbTextToWriteInOutput.AppendLine();
+                sbTextToWriteInOutput.AppendLine("Below are the Duplicate Emails in InputFile1");
+                foreach (var pMember in dupes1)
+                {
+                    sbTextToWriteInOutput.AppendLine(pMember.Key.Email);
+                }
+                sbTextToWriteInOutput.AppendLine();
+            }
+            if (dupes2.Count() > 0)
+            {
+                sbTextToWriteInOutput.AppendLine();
+                sbTextToWriteInOutput.AppendLine("Below are the Duplicate Emails in InputFile2");
+                foreach (var pMember in dupes2)
+                {
+                    sbTextToWriteInOutput.AppendLine(pMember.Key.Email);
+                }
+                sbTextToWriteInOutput.AppendLine();
+            }
+            sbTextToWriteInOutput.AppendLine("------------------------------------------------------------------");
+        }
+    }
+    static void FindCommonAndDifferentPanelMembers(bool isWriteToFile)
+    {
+        foreach (var data in PanelMembersLineInput1Unique)
+        {
+            if (PanelMembersLineInput2Unique.Any(x => x.Email == data.Email))
+            {
+                PanelMembersCommonInBoth.Add(data);
             }
             else
             {
-                neeeToAdd.Add(data);
+                PanelMembersInput1NotInInput2.Add(data);
             }
         }
-        foreach (var data in LineInput2Unique)
+        foreach (var data in PanelMembersLineInput2Unique)
         {
-            if (LineInput1.Any(x => x.Email == data.Email))
+            if (!PanelMembersLineInput1Unique.Any(x => x.Email == data.Email))
             {
-                commonInBoth2.Add(data);
-            }
-            else
-            {
-                neeeToInactivate.Add(data);
+                PanelMembersInput2NotInInput1.Add(data);
             }
         }
-        Console.WriteLine("Common in Both and count: " + commonInBoth.Count);
 
-        Console.WriteLine("Need to Add Count: " + neeeToAdd.Count);
-        foreach (var data in neeeToInactivate)
+
+        if (isWriteToFile)
         {
-            //   Console.WriteLine(data.Email+ "|" + data.Level);
+            sbTextToWriteInOutput.AppendLine();
+            sbTextToWriteInOutput.AppendLine("Distinct PanelMembers in Input1: " + PanelMembersLineInput1Unique.Count());
+            sbTextToWriteInOutput.AppendLine("Distinct PanelMembers in Input2: " + PanelMembersLineInput2Unique.Count());
+
+            sbTextToWriteInOutput.AppendLine();
+            sbTextToWriteInOutput.AppendLine("PanelMembers Common In Both Count: " + PanelMembersCommonInBoth.Count());
+            sbTextToWriteInOutput.AppendLine("PanelMembers in Input1 Not In Input2 Count: " + PanelMembersInput1NotInInput2.Count());
+            sbTextToWriteInOutput.AppendLine("PanelMembers in Input2 Not In Input1 Count: " + PanelMembersInput2NotInInput1.Count());
+            sbTextToWriteInOutput.AppendLine("------------------------------------------------------------------");
+            if (PanelMembersCommonInBoth.Any())
+            {
+                sbTextToWriteInOutput.AppendLine();
+                sbTextToWriteInOutput.AppendLine("PanelMembers Common In Both");
+                foreach (var data in PanelMembersCommonInBoth)
+                {
+                    sbTextToWriteInOutput.AppendLine(data.Id + '\t' + data.Email + '\t' + data.Level);
+                }
+                sbTextToWriteInOutput.AppendLine("------------------------------------------------------------------");
+            }
+
+            if (PanelMembersInput1NotInInput2.Any())
+            {
+                sbTextToWriteInOutput.AppendLine();
+                sbTextToWriteInOutput.AppendLine("PanelMembers in Input1, not in Input2");
+                foreach (var data in PanelMembersInput1NotInInput2)
+                {
+                    sbTextToWriteInOutput.AppendLine(data.Id + '\t' + data.Email + '\t' + data.Level);
+                }
+                sbTextToWriteInOutput.AppendLine("------------------------------------------------------------------");
+            }
+
+            if (PanelMembersInput2NotInInput1.Any())
+            {
+                sbTextToWriteInOutput.AppendLine();
+                sbTextToWriteInOutput.AppendLine("PanelMembers in Input2, not in Input1");
+                foreach (var data in PanelMembersInput2NotInInput1)
+                {
+                    sbTextToWriteInOutput.AppendLine(data.Id + '\t' + data.Email + '\t' + data.Level);
+                }
+                sbTextToWriteInOutput.AppendLine("------------------------------------------------------------------");
+            }
+
+            sbTextToWriteInOutput.AppendLine("------------------------------------------------------------------");
         }
+        
+    }
 
-        Console.WriteLine("Common in Both  2 and count: " + commonInBoth2.Count);
-        Console.WriteLine("Need to Inactivate count: " + neeeToInactivate.Count);
-
-
-        Console.WriteLine(LineInput1.Count);
-        Console.WriteLine(LineInput2.Count);
-
+    public static void WriteToFile()
+    {
+        string strOutput = sbTextToWriteInOutput.ToString();
+        File.WriteAllText(RepositoryProjectsPath + OutputFileRelativePath, strOutput);
+        Console.WriteLine("Output File Updated");
     }
 }
-class EmailLevel
+
+
+class PanelMemberModel
 {
+    public string Id { get; set; }
     public string Level { get; set; }
     public string Email { get; set; }
+
 }
--- MyCSharpApp/MyCSharpApp/PracticeCodes.cs
+++ MyCSharpApp/MyCSharpApp/PracticeCodes.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -12,10 +14,40 @@ namespace MyCSharpApp
     {
         public PracticeCodes()
         {
-            testJoins();
+            CheckConvertTryParse();
+            //TO PRACTICE CODE DONT FORGET TO SET THIS PROJECT AS THE STARTUP PROJECT
             Console.ReadKey();
         }
 
+
+        static void CheckConvertTryParse()
+        {
+            string strnum = "abc";
+            int num = Convert.ToInt32(strnum);
+            Console.WriteLine( num);
+
+            
+
+        }
+        static void DateRangeValidationTest()
+        {
+            //Dates are in the format MM/DD/YYYY
+            var startDate1 = Convert.ToDateTime("01/05/2021");
+            var endDate1 = Convert.ToDateTime("02/01/2021");
+
+            var startDate2 = Convert.ToDateTime("01/05/2021");
+            var endDate2 = Convert.ToDateTime("02/25/2021");
+
+            //Checking if Date2s lies between date1s
+            if ((startDate2 >= startDate1 && startDate2 <= endDate1) && (endDate2 >= startDate1 && endDate2 <= endDate1))
+            {
+                Console.WriteLine("In range");
+            }
+            else
+            {
+                Console.WriteLine("Not in Range");
+            }
+        }
         static void testJoins()
         {
             List<ModelData> md1 = new List<ModelData>();
@@ -135,6 +167,40 @@ namespace MyCSharpApp
             Console.WriteLine(op);
 
         }
+        static void tryWriteToFile()
+        {
+            var exePath = Assembly.GetExecutingAssembly().Location;
+            int lastIndexOfDebug = exePath.LastIndexOf("Debug");
+            string filePath = exePath.Substring(0, lastIndexOfDebug) + @"Debug\Files\MyLogger.txt";
+            //File.AppendAllText(filePath, Environment.NewLine);
+            //File.AppendAllText(filePath, "This is a line at " + DateTime.Now.ToShortTimeString());
+            FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Please prepend this at the beginning " + DateTime.Now.ToLongTimeString() + " " + DateTime.Now.Second.ToString());
+            sb.AppendLine("-----------");
+            PrependString(sb.ToString(), file);
+
+
+        }
+        public static void PrependString(string value, FileStream file)
+        {
+            var buffer = new byte[file.Length];
+
+            while (file.Read(buffer, 0, buffer.Length) != 0)
+            {
+            }
+
+            if (!file.CanWrite)
+                throw new ArgumentException("The specified file cannot be written.", "file");
+
+            file.Position = 0;
+            var data = Encoding.UTF8.GetBytes(value);
+            file.SetLength(buffer.Length + data.Length);
+            file.Write(data, 0, data.Length);
+            file.Write(buffer, 0, buffer.Length);
+        }
+
+      
 
 
     }
