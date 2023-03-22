b3a7b8b10ad0a8d23baea956a13c4c2116336bb5
subhadeb <subhadeb93@gmail.com>
2023-03-22 18:49:13 +0530

    SQL script creation for Update

--- MyCSharpApp/16_CreateSQLScripts/Program.cs
+++ MyCSharpApp/16_CreateSQLScripts/Program.cs
@@ -85,8 +85,9 @@ class Program
         SBQueryToWrite = new StringBuilder();
         int input = 0;
         Console.WriteLine("Table Name: " + TableName);
-        Console.WriteLine("Do you want Create table Statement Press Y For Yes any other Key For No");
-        if (Console.ReadLine().ToLower() == "y")
+        Console.WriteLine("Press 'C' for Create+Insert table statement; Press 'I' for Insert(only) statement; Press 'U' for Update statement");
+        string inputLowerCase = Console.ReadLine().ToLower();
+        if (inputLowerCase == "c")
         {
 
             Console.WriteLine("Datatype Input:[1: INT] [2: VARCHAR(50)] [3: VARCHAR(150)] [4 DATETIME]");
@@ -132,50 +133,109 @@ class Program
             SBQueryToWrite.AppendLine();
         }
 
-
-        var insertStatementLine1 = "INSERT INTO " + TableName + "\n(";
-        var insertStatementLine2 = string.Join(",", columnNamesArray) + "\n) VALUES";
-
-        SBQueryToWrite.AppendLine(insertStatementLine1);
-        SBQueryToWrite.AppendLine(insertStatementLine2);
-        //Assiming Each Input Line was in the format(Tab or \t after 3): 3 Join our Online Learning Community
-        if (ListStrLineElements != null && ListStrLineElements.Any())
+        if (inputLowerCase == "c" || inputLowerCase == "i") 
         {
-            foreach (var lineItem in ListStrLineElements)
+            var insertStatementLine1 = "INSERT INTO " + TableName + "\n(";
+            var insertStatementLine2 = string.Join(",", columnNamesArray) + "\n) VALUES";
+
+            SBQueryToWrite.AppendLine(insertStatementLine1);
+            SBQueryToWrite.AppendLine(insertStatementLine2);
+            //Assiming Each Input Line was in the format(Tab or \t after 3): 3 Join our Online Learning Community
+            if (ListStrLineElements != null && ListStrLineElements.Any())
             {
-                var dataArray = lineItem.Split('\t');
-                var dataListWithQute = new List<string>();
-                foreach (var data in dataArray)
+                foreach (var lineItem in ListStrLineElements)
                 {
-                    var dataAfterRemovingSpecialChars = Regex.Replace(data, @"[^0-9a-zA-Z ,./:()-_@&]+", "");
-                    if (dataAfterRemovingSpecialChars != "NULL")
+                    var dataArray = lineItem.Split('\t');
+                    var dataListWithQute = new List<string>();
+                    foreach (var data in dataArray)
                     {
-                        dataListWithQute.Add("'" + dataAfterRemovingSpecialChars + "'");
+                        var dataAfterRemovingSpecialChars = Regex.Replace(data, @"[^0-9a-zA-Z ,./:()-_@&]+", "");
+                        if (dataAfterRemovingSpecialChars != "NULL")
+                        {
+                            dataListWithQute.Add("'" + dataAfterRemovingSpecialChars + "'");
+                        }
+                        else
+                        {
+                            //For NULL, we dont need Quotation.
+                            dataListWithQute.Add(dataAfterRemovingSpecialChars);
+                        }
                     }
-                    else
+                    SBQueryToWrite.AppendLine("(" + string.Join(",", dataListWithQute) + "),");
+                    if (ListStrLineElements.IndexOf(lineItem) > 0 && ListStrLineElements.IndexOf(lineItem) % 900 == 0)
                     {
-                        //For NULL, we dont need Quotation.
-                        dataListWithQute.Add(dataAfterRemovingSpecialChars);
+                        //Insert only allows upto 1000 Elements at a time
+                        if (SBQueryToWrite[SBQueryToWrite.Length - 3] == ',')
+                        {
+                            SBQueryToWrite.Remove(SBQueryToWrite.Length - 3, 1);
+                        }
+                        SBQueryToWrite.AppendLine();
+                        SBQueryToWrite.AppendLine();
+                        SBQueryToWrite.AppendLine(insertStatementLine1);
+                        SBQueryToWrite.AppendLine(insertStatementLine2);
                     }
                 }
-                SBQueryToWrite.AppendLine("(" + string.Join(",", dataListWithQute) + "),");
-                if (ListStrLineElements.IndexOf(lineItem) > 0 && ListStrLineElements.IndexOf(lineItem) % 900 == 0) {
-                    //Insert only allows upto 1000 Elements at a time
+                if (SBQueryToWrite[SBQueryToWrite.Length - 3] == ',')
+                {
+                    SBQueryToWrite.Remove(SBQueryToWrite.Length - 3, 1);
+                }
+            }
+        }
+        if (inputLowerCase == "u")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Is First Column '" + columnNamesArray[0] + "' the Primary-Key/Where-Clause-Colum? Press 'Y' for Yes any other key for No");
+            var isFirstColumnPrimaryKey = Console.ReadLine().ToLower() == "y";
+            
+            if (ListStrLineElements != null && ListStrLineElements.Any())
+            {
+                string firstColumnVal = null;
+                foreach (var lineItem in ListStrLineElements)
+                {
+                    SBQueryToWrite.AppendLine("UPDATE " + TableName);
+                    SBQueryToWrite.AppendLine("SET");
+                    var dataArray = lineItem.Split('\t');
+                    var dataListWithQute = new List<string>();
+
+                    for (int i = 0; i < dataArray.Length; i++)
+                    {
+                        
+                        var data = dataArray[i];
+                        var columnName = columnNamesArray[i];
+
+                        var dataAfterRemovingSpecialChars = Regex.Replace(data, @"[^0-9a-zA-Z ,./:()-_@&]+", "");
+                        if (dataAfterRemovingSpecialChars != "NULL")
+                        {
+                            dataAfterRemovingSpecialChars = "'" + dataAfterRemovingSpecialChars + "'";
+                        }
+                        if (i == 0)
+                        {
+                            firstColumnVal = dataAfterRemovingSpecialChars;
+                        }
+                        if (i > 0 || (i == 0 && !isFirstColumnPrimaryKey))
+                        {
+                            SBQueryToWrite.AppendLine(columnName + " = " + dataAfterRemovingSpecialChars + ",");
+                        }
+                    }
                     if (SBQueryToWrite[SBQueryToWrite.Length - 3] == ',')
                     {
                         SBQueryToWrite.Remove(SBQueryToWrite.Length - 3, 1);
                     }
+                    if (isFirstColumnPrimaryKey && !string.IsNullOrEmpty(firstColumnVal))
+                    {
+                        SBQueryToWrite.AppendLine("WHERE "+ columnNamesArray[0] + " = " + firstColumnVal + "");
+                    }
+                    else 
+                    {
+                        SBQueryToWrite.AppendLine("WHERE 1 = 0");
+                    }
+                    SBQueryToWrite.AppendLine("---------------------------------------------------------------");
                     SBQueryToWrite.AppendLine();
-                    SBQueryToWrite.AppendLine();
-                    SBQueryToWrite.AppendLine(insertStatementLine1);
-                    SBQueryToWrite.AppendLine(insertStatementLine2);
+
                 }
-            }
-            if (SBQueryToWrite[SBQueryToWrite.Length - 3] == ',')
-            {
-                SBQueryToWrite.Remove(SBQueryToWrite.Length - 3, 1);
+               
             }
         }
+
     }
     static void WriteToOutputFile()
     {
